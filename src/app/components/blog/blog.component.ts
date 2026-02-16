dc4d6525986ff2ebc47f85f238fa09a25c5ed6b7
diegozsr1 <diegozsr1@gmail.com>
2026-02-16 11:19:03 +0100

    Iniciado array de noticias con 2 noticias

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { INoticia } from '../../interfaces/inoticia.interface';
 
 @Component({
   selector: 'app-blog',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  arrayNoticias: INoticia[] = [
+    {
+      titulo: 'Primera noticia',
+      imagen: 'https://images.ecestaticos.com/kusNwYwGY5Hx72h2immY2W377d0=/0x0:2272x1552/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F9e6%2F637%2Fe9d%2F9e6637e9d75e375b63a05ff809178b34.jpg',
+      texto: 'Contenido de la primera noticia',
+      fecha: '2026-02-16'
+    },
+    {
+      titulo: 'segunda noticia',
+      imagen: 'https://cdn-5.motorsport.com/images/amp/0L1yy9V2/s1000/marc-marquez-ducati-3.jpg',
+      texto: 'Contenido de la segunda noticia',
+      fecha: '2026-02-16'
+    },
+  ]
 
 }
